efc1217b216898f2ed033af5dd84880753d872b4
Harry McDonald <harrymcdonald123@gmail.com>
2019-09-11 23:52:07 +1000

    Aspect ratio filtering is now working - rather than hard threshold on area. This should make it intelligent to photos that aren't in the basic folder and may be taken from closer to or futher away from the image.

--- BT1.m
+++ BT1.m
@@ -1,7 +1,7 @@
 %% Extracting edges
 clear all
 close all
-orig = imread('C:\Users\Harry\Documents\MATLAB\YEAR 3\SEM 2\METR4202\PS2_Images\PS2 Images\Basic\Simple19.png'); %import image
+orig = imread('C:\Users\Harry\Documents\MATLAB\YEAR 3\SEM 2\METR4202\PS2_Images\PS2 Images\Basic\Simple20.png'); %import image
 im1 = rgb2gray(orig); %make grayscale
 figure(1)
 levels = multithresh(im1,5);
@@ -12,13 +12,20 @@ im2 = imdilate(im1,se); %Dilate image to fill gaps based on structural element s
 im3 = imfill(im2,'holes'); % fill in card face
 im3 = imerode(im3,se); % erode back to normal size
 figure; imshow(im3)
-imageRegionAnalyzer(im3)
-area_range = [62000,75000];% From the angle and distance of the photos provided this was the min and max range of the cards
-im3 = bwareafilt(im3,area_range); % Filter out all objects with area < or > area_range
+%imageRegionAnalyzer(im3)
+[im_out,properties,goodIm_index] = filterRegions(im3);
+% area_range = [62000,75000];% From the angle and distance of the photos provided this was the min and max range of the cards
+% im3 = bwareafilt(im3,area_range); % Filter out all objects with area < or > area_range
+for k = 1:length(goodIm_index)
+    area = properties(goodIm_index(k)).Area
+    area_up = area+area*0.05;
+    area_low = area-area*0.05;
+    filt_im = bwareafilt(im3,[area_low,area_up]);
+end
 figure(4)
 imshow(orig);
 hold on;
-[B,L,n,A] = bwboundaries(im3);
+[B,L,n,A] = bwboundaries(filt_im);
 for ii = 1:length(B)
     data = B{ii};
     plot(data(:,2),data(:,1),'green','LineWidth',2)
